e5484357811c46e78237071383cb5b0ed0cf1e97
VladimirOvhchinnikov <120370179+VladimirOvhchinnikov@users.noreply.github.com>
2023-09-17 15:14:32 +0300

    Create mymath_test.go

---
+++ mymath_test.go
@@ -0,0 +1,95 @@
+package mymath
+
+import (
+	"math"
+	"testing"
+)
+
+func TestAbs(t *testing.T) {
+	tests := []struct {
+		input  float64
+		output float64
+	}{
+		{-1, 1},
+		{0, 0},
+		{1, 1},
+	}
+
+	for _, tt := range tests {
+		got := Abs(tt.input)
+		if got != tt.output {
+			t.Errorf("Abs(%v) = %v; want %v", tt.input, got, tt.output)
+		}
+	}
+}
+
+func TestAcos(t *testing.T) {
+	tests := []struct {
+		input  float64
+		output float64
+	}{
+		{1, 0},
+		{0, math.Pi / 2},
+		{-1, math.Pi},
+	}
+
+	for _, tt := range tests {
+		got := Acos(tt.input)
+		if got != tt.output {
+			t.Errorf("Acos(%v) = %v; want %v", tt.input, got, tt.output)
+		}
+	}
+}
+
+func TestAcosh(t *testing.T) {
+	tests := []struct {
+		input  float64
+		output float64
+	}{
+		{1, 0},
+		{math.Cosh(1), 1},
+	}
+
+	for _, tt := range tests {
+		got := Acosh(tt.input)
+		if got != tt.output {
+			t.Errorf("Acosh(%v) = %v; want %v", tt.input, got, tt.output)
+		}
+	}
+}
+
+func TestAsin(t *testing.T) {
+	tests := []struct {
+		input  float64
+		output float64
+	}{
+		{0, 0},
+		{1, math.Pi / 2},
+		{-1, -math.Pi / 2},
+	}
+
+	for _, tt := range tests {
+		got := Asin(tt.input)
+		if got != tt.output {
+			t.Errorf("Asin(%v) = %v; want %v", tt.input, got, tt.output)
+		}
+	}
+}
+
+func TestAsinh(t *testing.T) {
+	tests := []struct {
+		input  float64
+		output float64
+	}{
+		{0, 0},
+		{math.Sinh(1), 1},
+		{-math.Sinh(1), -1},
+	}
+
+	for _, tt := range tests {
+		got := Asinh(tt.input)
+		if got != tt.output {
+			t.Errorf("Asinh(%v) = %v; want %v", tt.input, got, tt.output)
+		}
+	}
+}
